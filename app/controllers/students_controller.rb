be3d55cb5bb02afceb04c3c8db197cd8db3965a7
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-06-20 01:02:50 +0300

    Completed lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    name=params[:name]
+    if name==nil
+      students = Student.all
+      render json: students
+    else
+      student = Student.all.filter {|student| student.first_name.downcase==name || student.last_name.downcase==name}
+      render json: student
+    end
   end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+ 
 
 end
